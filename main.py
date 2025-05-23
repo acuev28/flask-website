effaf95848fd1be8d69e812feb5b31a915b21dbc
acuev28 <alejandrocuevas188@gmail.com>
2025-05-23 13:59:56 -0500

    Add style and images, update main.py and index.html

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+@app.route('/')
+def website():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
 
